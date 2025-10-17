69fd342f3178b10e5a087df7cf293976562ec2ad
Jo Youngpyeong <80379900+ypMarkJo@users.noreply.github.com>
2025-10-17 19:14:14 +0900

    Time: 4 ms (15.69%), Space: 12.4 MB (82.2%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -4,25 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
         remains=0
         split=0
-        i=0
-        while i < len(s)-1:
-            print(s[i]+","+s[i+1]+",remains: ",remains,",split: ",split)
-            if s[i]==s[i+1]:
-                if s[i+1]=='R':
+        for i in range(len(s)):
+                if s[i]=='R':
                     remains+=1
                 else:
                     remains-=1
-                if remains==0:
-                    split+=1
-                    i+=1
 
-            elif s[i]!=s[i+1]:
                 if remains==0:
                     split+=1
-                    i+=1
-            i+=1        
-                    
+                              
         return split
