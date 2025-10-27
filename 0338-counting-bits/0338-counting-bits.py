cc376eddd131fcbf14eb8bb98a3277f19f41d071
Jo Youngpyeong <80379900+ypMarkJo@users.noreply.github.com>
2025-10-27 20:25:55 +0900

    Time: 7 ms (76.36%), Space: 17.9 MB (12.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,11 +1,22 @@
 class Solution(object):
-    def countBits(self, n):
+    # def countBits(self, n):
+    #     """
+    #     count 방식
+    #     :type n: int
+    #     :rtype: List[int]
+    #     """
+    #     output=[]
+    #     for i in range(n+1):
+    #         output.append(bin(i)[2:].count('1'))
+    #     return output
+
+     def countBits(self, n):
         """
+        dp 방식
         :type n: int
         :rtype: List[int]
         """
-        output=[]
+        dp=[0]*(n+1)
         for i in range(n+1):
-            output.append(bin(i)[2:].count('1'))
-        return output
-
+            dp[i]=dp[i>>1]+(i&1)
+        return dp
