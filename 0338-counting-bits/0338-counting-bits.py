d6361f4c516d657a4045a6cf97f5bb0a5eaa0079
Jo Youngpyeong <80379900+ypMarkJo@users.noreply.github.com>
2025-10-27 20:26:17 +0900

    Time: 6 ms (82.32%), Space: 17.9 MB (47.38%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -17,6 +17,6 @@ class Solution(object):
         :rtype: List[int]
         """
         dp=[0]*(n+1)
-        for i in range(n+1):
+        for i in range(1,n+1):
             dp[i]=dp[i>>1]+(i&1)
         return dp
