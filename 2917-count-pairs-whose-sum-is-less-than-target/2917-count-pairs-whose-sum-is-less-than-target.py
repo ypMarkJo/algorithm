7311244f2dc2d1c16dd2e2ceac9cc8b311737bb7
Jo Youngpyeong <80379900+ypMarkJo@users.noreply.github.com>
2025-10-28 17:34:09 +0900

    Time: 13 ms (15.92%), Space: 12.6 MB (13.18%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.py
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.py
@@ -1,14 +1,32 @@
 class Solution(object):
+    # def countPairs(self, nums, target):
+    #     """
+    #     내 풀이 : 불필요한 반복구간 수행이 있음.
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: int
+    #     """
+    #     count=0
+    #     n=len(nums)
+    #     for i in range(n):
+    #         for j in range(i+1,n):
+    #                 if target>nums[j]+nums[i]:
+    #                     count+=1
+    #     return count
+
     def countPairs(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
+        정석 풀이: 필요한 구간만 돔.
         """
-        count=0
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
-                    if target>nums[j]+nums[i]:
-                        count+=1
-        return count
+        nums.sort()
+        print(nums)
+        ans=0
+        i,j=0,len(nums)-1
+        
+        while i<j:
+            if nums[i] + nums[j] < target:
+                ans += j - i
+                i += 1
+            else:
+                j -= 1
+        return ans
