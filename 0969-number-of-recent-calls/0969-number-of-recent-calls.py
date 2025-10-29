83549c315f4ea6e44f68aa13b578f8918dc19d49
Jo Youngpyeong <80379900+ypMarkJo@users.noreply.github.com>
2025-10-29 17:05:35 +0900

    Time: 90 ms (24.47%), Space: 17 MB (99.02%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -4,20 +4,31 @@ class RecentCounter(object):
 
         self.recents=[]
 
+    # def ping(self, t):
+    #     """
+    #     반복문 방식
+    #     :type t: int
+    #     :rtype: int
+    #     """
+    #     if t:
+    #         self.recents.append(t)
+    #         end=len(self.recents)-1
+    #         i=end
+    #         while i>=0 and t-3000 <= self.recents[i]<=t:
+    #             i-=1
+                
+    #     return end-i
+    
     def ping(self, t):
         """
+        첫항 삭제 방식
         :type t: int
         :rtype: int
         """
-        if t:
-            self.recents.append(t)
-            end=len(self.recents)-1
-            i=end
-            while i>=0 and t-3000 <= self.recents[i]<=t:
-                i-=1
-                
-        return end-i
-        
+        self.recents.append(t)
+        while self.recents[0]<t-3000:
+            del self.recents[0]
+        return len(self.recents)
 
 
 # Your RecentCounter object will be instantiated and called as such:
